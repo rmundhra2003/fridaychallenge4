ba798eb899ce2a89d275fd083079b7e9ba18c6e4
Rekha Mundhra <rmundhra2003@yahoo.com>
2019-05-10 10:21:30 -0400

    initial commit

---
+++ src/main/java/com/example/fridaychallenge4/HomeController.java
@@ -0,0 +1,37 @@
+package com.example.fridaychallenge4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String homePage(){
+        return "index";
+    }
+    @RequestMapping("/git")
+    public String gitPage(){
+        return "git";
+    }
+    @RequestMapping("/design")
+    public String designPage(){
+        return "design";
+    }
+    @RequestMapping("/java")
+    public String javaPage(){
+        return "java";
+    }
+    @RequestMapping("/oop")
+    public String oopPage(){
+        return "oop";
+    }
+    @RequestMapping("/cssboot")
+    public String cssbootPage(){
+        return "cssboot";
+    }
+    @RequestMapping("/bootcamp")
+    public String bootcampPage(){
+        return "bootcamp";
+    }
+}
