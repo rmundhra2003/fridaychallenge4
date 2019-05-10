af3fc898d206129eca3565aa2c833823707d0b12
Rekha Mundhra <rmundhra2003@yahoo.com>
2019-05-10 09:32:05 -0400

    Initial commit

---
+++ src/main/java/com/example/fridaychallenge4/Fridaychallenge4Application.java
@@ -0,0 +1,13 @@
+package com.example.fridaychallenge4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fridaychallenge4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fridaychallenge4Application.class, args);
+	}
+
+}
